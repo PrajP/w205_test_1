2f12c1382d05b8d60753664c7b2458bada4224c3
PrajP <prajakta.p@ischool.berkeley.edu>
2016-12-06 15:17:03 -0800

    change dir

--- W205_5_test_1/Project-1/src/scripts/analytics_dataload.sh
+++ W205_5_test_1/Project-1/src/scripts/analytics_dataload.sh
@@ -5,13 +5,13 @@
 NOW=$(date +"%Y-%m-%d")
 echo $NOW
 
-dir1=$(/data/w205_test_1/W205_5_test_1/Project-1)
+dir1=$'data/w205_test_1/W205_5_test_1/Project-1'
 
-cd $dir1/src/scripts
+cd /$dir1/src/scripts
 #cd /data/src/scripts
 echo $(pwd)
 
-
+su - w205
 
 echo 'Make directory structures for all hdfs files.'
 hdfs dfs -mkdir -p /user/w205/product_features/
@@ -24,9 +24,9 @@ echo 'Directory structures completed.'
 
 
 echo 'Removing files temporary files.'
-rm -r $dir1/data/analysis_output/apple_result_temp.tsv
-rm -r $dir1/data/analysis_output/google_result_temp.tsv
-rm -r $dir1/data/analysis_output/samsung_result_temp.tsv
+rm -r /$dir1/data/analysis_output/apple_result_temp.tsv
+rm -r /$dir1/data/analysis_output/google_result_temp.tsv
+rm -r /$dir1/data/analysis_output/samsung_result_temp.tsv
 
 
 sleep 5
@@ -63,17 +63,17 @@ echo 'Hadoop files removed.'
 
 echo 'Copy clean analysis files to hdfs.'
 
-hdfs dfs -put $dir1/data/analysis_output/apple_result_temp.tsv /user/w205/analysis_data/apple/apple_result.tsv
-hdfs dfs -put $dir1/data/analysis_output/samsung_result_temp.tsv /user/w205/analysis_data/samsung/samsung_result.tsv
-hdfs dfs -put $dir1/data/analysis_output/google_result_temp.tsv /user/w205/analysis_data/google/google_result.tsv
-hdfs dfs -put $dir1/data/analysis_output/features.txt /user/w205/analysis_data/features/features.txt
+hdfs dfs -put /$dir1/data/analysis_output/apple_result_temp.tsv /user/w205/analysis_data/apple/apple_result.tsv
+hdfs dfs -put /$dir1/data/analysis_output/samsung_result_temp.tsv /user/w205/analysis_data/samsung/samsung_result.tsv
+hdfs dfs -put /$dir1/data/analysis_output/google_result_temp.tsv /user/w205/analysis_data/google/google_result.tsv
+hdfs dfs -put /$dir1/data/analysis_output/features.txt /user/w205/analysis_data/features/features.txt
 
 sleep 5
 
 echo 'Files moved to final destination.'
 
 #cd /data/src/scripts/
-cd $dir1/src/scripts
+cd /$dir1/src/scripts
 
 echo $(pwd)
 
